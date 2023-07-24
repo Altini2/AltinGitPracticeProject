d41c1f194cf7358243f2d1d36c10d4522811bd1b
Plus Computers <rahitialtin8@gmai.com>
2023-07-24 18:55:22 +0200

    1t version of the game

---
+++ src/Day1Code.java
@@ -0,0 +1,5 @@
+public class Day1Code {
+
+    //some code for my game
+
+}
---
+++ src/Day2Code.java
@@ -0,0 +1,10 @@
+public class Day2Code {
+    public static void main(String[] args) {
+
+
+        System.out.println("Day2 code  of my game , I created some avatars today ");
+
+
+
+    }
+}
