46b37f2b9bc27f8329064db6cf0bb2343a28087f
Plus Computers <rahitialtin8@gmai.com>
2023-07-24 19:35:08 +0200

    i did some fixes about smth

--- src/Day2Code.java
+++ src/Day2Code.java
@@ -4,7 +4,7 @@ public class Day2Code {
 
         System.out.println("Day2 code  of my game , I created some avatars today ");
 
-
+        System.out.println("i added more code to fix ");
 
     }
 }
